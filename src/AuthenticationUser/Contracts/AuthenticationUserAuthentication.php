629dc3cd6c06862b4af9e60a854b32205cf231e0
Toby Twigger <tobytwigger1@gmail.com>
2020-12-12 10:50:24 +0000

    Initial Commit

---
+++ src/AuthenticationUser/Contracts/AuthenticationUserAuthentication.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace BristolSU\Auth\AuthenticationUser\Contracts;
+
+use BristolSU\Auth\AuthenticationUser\User;
+
+/**
+ * Contract to set or resolve a logged in user
+ */
+interface UserAuthentication
+{
+
+    /**
+     * Get the currently set user
+     *
+     * @return User|null Returns the user, or null if no user found
+     */
+    public function getUser(): ?User;
+
+    /**
+     * Set the currently logged in user
+     *
+     * @param User $user User to log in
+     * @return void
+     */
+    public function setUser(User $user);
+
+    /**
+     * Log out of the current user
+     *
+     * @return void
+     */
+    public function logout(): void;
+
+}
