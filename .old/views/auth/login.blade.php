e2645f5705f94b650add17ba0deddca1d9acdd83
Toby Twigger <tobytwigger1@gmail.com>
2021-01-12 02:08:52 +0000

    Remove all old code

--- .old/views/auth/login.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.portal')
-
-@section('title', 'Login')
-
-@section('app-content')
-
-    <a href="/theme/demo">Theme demo</a>
-@endsection
--- .old/views/auth/passwords/email.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.portal')
-
-@section('title', 'Password Reset')
-
-@section('app-content')
-<p-password-email-form
-    status="{{session('status', null)}}">
-</p-password-email-form>
-@endsection
